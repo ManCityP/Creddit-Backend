abc1aa17f236b46eaedecc118711679858b8e876
Ahmed Mostafa Mousa <24P0030@eng.asu.edu.eg>
2025-10-31 16:39:11 +0200

    Add files via upload

--- src/main/java/com/crdt/MediaType.java
+++ src/main/java/com/crdt/MediaType.java
@@ -1,32 +1,3 @@
-package com.crdt;
-
-import com.crdt.users.Gender;
-
-public enum MediaType {
-    IMAGE("Image"),
-    VIDEO("Video"),
-    AUDIO("Audio"),
-    OTHER("Other");
-
-    private final String mediaType;
-
-    MediaType(String mt) {
-        this.mediaType = mt;
-    }
-
-    public static MediaType toMediaType(String s) {
-        if(s.equalsIgnoreCase("Image"))
-            return IMAGE;
-        else if(s.equalsIgnoreCase("Video"))
-            return VIDEO;
-        if(s.equalsIgnoreCase("Audio"))
-            return AUDIO;
-        else if(s.equalsIgnoreCase("Other"))
-            return OTHER;
-        else
-            return null;
-    }
-    public String getMediaType() {
-        return this.mediaType;
-    }
+public enum MediaType{
+    IMAGE , VIDEO , AUDIO , OTHER;
 }
