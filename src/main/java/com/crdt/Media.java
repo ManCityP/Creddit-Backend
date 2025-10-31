baed844d5fc08ace3a1cb46928e210d9b7c1aa91
Fris233 <baefris@gmail.com>
2025-10-31 14:56:00 +0200

    Added skeletons for Admin and Media classes

--- src/main/java/com/crdt/Media.java
+++ src/main/java/com/crdt/Media.java
@@ -1,22 +1,22 @@
 package com.crdt;
 
-import java.awt.*;
+
 
 public class Media {
-    private PageAttributes.MediaType mediaType;
+    private MediaType mediaType;
     private String mediaUrl;
 
-    public Media(PageAttributes.MediaType mediaType, String mediaUrl){
+    public Media(MediaType mediaType, String mediaUrl){
         this.mediaType = mediaType;
         this.mediaUrl = mediaUrl;
     }
-    public PageAttributes.MediaType GetType(){
+    public MediaType GetType(){
         return this.mediaType;
     }
     public String GetURL(){
         return this.mediaUrl;
     }
-    public void SetType(PageAttributes.MediaType mediaType){
+    public void SetType(MediaType mediaType){
         this.mediaType = mediaType;
     }
     public void SetURL(String URL){
