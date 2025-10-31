105ddc4ff912f8105fc83ff1a098f52d60f4e0aa
Fris233 <baefris@gmail.com>
2025-10-31 14:30:02 +0200

    Added skeletons for Admin and Media classes

--- src/main/java/com/crdt/Media.java
+++ src/main/java/com/crdt/Media.java
@@ -1,4 +1,25 @@
 package com.crdt;
 
+import java.awt.*;
+
 public class Media {
+    private PageAttributes.MediaType mediaType;
+    private String mediaUrl;
+
+    public Media(PageAttributes.MediaType mediaType, String mediaUrl){
+        this.mediaType = mediaType;
+        this.mediaUrl = mediaUrl;
+    }
+    public PageAttributes.MediaType GetType(){
+        return this.mediaType;
+    }
+    public String GetURL(){
+        return this.mediaUrl;
+    }
+    public void SetType(PageAttributes.MediaType mediaType){
+        this.mediaType = mediaType;
+    }
+    public void SetURL(String URL){
+        this.mediaUrl= URL;
+    }
 }
--- src/main/java/com/crdt/users/Admin.java
+++ src/main/java/com/crdt/users/Admin.java
@@ -1,4 +1,19 @@
 package com.crdt.users;
 
-public class Admin {
+import com.crdt.Media;
+
+import java.sql.Timestamp;
+
+public class Admin extends User {
+    public Admin(int id, String username, String email, String password, Gender gender, String bio, Media pfp, Timestamp timeCreated) {
+        super(id, username, email, password, gender, bio, pfp, timeCreated);
+    }
+
+   public void BanMember(User user) {
+
+   }
+   public void UnbanMember(User user){
+
+    }
+
 }
