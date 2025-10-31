95a9e88293c0797ae57b4a0e4dd8c81b3316f633
Ahmed Mostafa Mousa <24P0030@eng.asu.edu.eg>
2025-10-31 16:13:49 +0200

    added

--- src/main/java/com/crdt/users/Moderator.java
+++ src/main/java/com/crdt/users/Moderator.java
@@ -1,4 +1,31 @@
 package com.crdt.users;
 
 public class Moderator {
+    private ArrayList<Subcreddit> subcreddits;
+
+    public Moderator(int id, String firstName, String lastName, String email,
+                     Gender gender, String bio, Media profileMedia,
+                     Timestamp joinDate, ArrayList<Subcreddit> subcreddits) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.bio = bio;
+        this.profileMedia = profileMedia;
+        this.joinDate = joinDate;
+        this.subcreddits = subcreddits != null ? subcreddits : new ArrayList<>();
+    }
+    public ArrayList<Subcreddit> GetSubcreddits() {
+        return subcreddits;
+    }
+    public void KickMember(User user) {
+    }
+    public boolean VerifyModeration(Subcreddit subcreddit) {
+    }
+    public void addSubcreddit(Subcreddit subcreddit) {
+    }
+
+
+
 }
