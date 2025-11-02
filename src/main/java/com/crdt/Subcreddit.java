aec4c49fbba1be85356970b7b2e98c581816da53
NotAlyy7 <notaly66@gmail.com>
2025-11-02 20:37:50 +0200

    lalalaa

--- src/main/java/com/crdt/Subcreddit.java
+++ src/main/java/com/crdt/Subcreddit.java
@@ -7,36 +7,26 @@ import java.util.ArrayList;
 
 public class Subcreddit {
     private int id;
-    private ArrayList members;
+    private ArrayList<User> members;
     private String name ;
     private String description ;
     private Timestamp timecreated;
     private User creator;
     private Media subLogo;
     private boolean isPrivate;
-    private ArrayList moderators;
-    private String rules;
-    private ArrayList posts;
 
 
 
-    public Subcreddit(int id, ArrayList members, String name, String description, Timestamp timecreated, User creator,  Media logo, boolean isPrivate, ArrayList moderators, String rules ){
+    public Subcreddit(int id, ArrayList members, String name, String description, Timestamp timecreated, User creator,  Media logo, boolean isPrivate){
         this.id = id;
         this.members = members;
         this.name = name;
         this.timecreated = timecreated;
         this.isPrivate = isPrivate;
-        this.moderators = moderators;
-        this.rules = rules;
         this.creator = creator;
         this.subLogo = logo;
-        this.posts = null;
     }
     public ArrayList GetMembers(){
-        return members;
-    }
-
-    public ArrayList GetModerators(){
         return members;
     }
 
@@ -56,24 +46,12 @@ public class Subcreddit {
         return timecreated;
     }
 
-    public String GetRules() {
-        return rules;
-    }
-
     public User GetCreator() {
         return creator;
     }
 
      public Media GetLogo() {
         return subLogo;
-    }
-
-
-    void AddPost (Post post){
-        posts.add(post);
-    }
-    void RemovePost (Post post){
-        posts.remove(post);
     }
 
     void AddMembers (User user){
@@ -83,24 +61,12 @@ public class Subcreddit {
         members.remove(user);
     }
 
-    void AssignModerator (User user){
-        moderators.add(user);
-    }
-    void RemoveModerator (User user){
-        moderators.remove(user);
-    }
-
     void UpdateDescription (String description){
         this.description = description;
     }
 
      void UpdateLogo (Media logo){
         this.subLogo = logo;
-    }
-
-
-    void UpdateRules (String rules){
-        this.rules = rules;
     }
 }
 
